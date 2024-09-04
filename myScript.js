aca15f7e8cca37da461c300fe0732623f8776ff5
Luiz Felipe <felipemonteiro78@gmail.com>
2024-09-04 08:14:20 -0300

    Projeto finalizado

--- myScript.js
+++ myScript.js
@@ -6,6 +6,7 @@ function enter(){
     inputGrid = Math.floor(inputGrid);
     console.log(inputGrid);
     grid(inputGrid);
+
     
 
 }
@@ -15,6 +16,8 @@ document.getElementById("getNumber").addEventListener('keydown', function(event)
         enter();
     }
 });
+
+
 
 function grid(inputGrid){
         for(let i = 0; i < inputGrid; i++){
@@ -30,7 +33,15 @@ function grid(inputGrid){
             const newDiv = document.createElement("div");
             newDiv.className = "divD " + y;
             newRow.appendChild(newDiv);
+            newDiv.addEventListener("mouseover", changeColor);
         }
 
     }
 }     
+
+    function changeColor(event){
+        event.target.style.backgroundColor = "grey"; 
+    }
+
+
+
